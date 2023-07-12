c40a082ca20e1c6ac9d21c4c5cc1c74ddf60686d
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-12 10:59:08 +0200

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,1 @@
-    def is_dealer_bust(self):
-        return self.dealer_hand.value > 21
-
-    def is_player_win(self):
-        return self.player_hand.value > self.dealer_hand.value
+import random
